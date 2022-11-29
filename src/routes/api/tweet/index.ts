77345bed14922d8a1593be6cf670460f9fca0f0e
Tejas Kumar <tejas@tejas.qa>
2022-11-29 21:21:37 +1100

    Initial Commit

---
+++ src/routes/api/tweet/index.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from "@builder.io/qwik-city";
+import "node-fetch"
+
+export const onPost: RequestHandler = async ({ request }) => {
+    const body = JSON.parse(await request.text());
+
+    const res = await fetch("https://api.twitter.com/2/tweets", {
+        method: "POST",
+        headers: {
+            Authorization: request.headers.get('Authorization')!,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            text: body.text,
+        }),
+    }).then(r => r.json())
+
+    return res;
+};
